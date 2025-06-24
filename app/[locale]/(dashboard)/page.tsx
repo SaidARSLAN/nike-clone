b473ec1de08f67abb7510af164a4252600c4e318
SaidARSLAN <said.arslan@tecnovy.com>
2025-06-25 00:42:38 +0300

    main

--- app/[locale]/(dashboard)/page.tsx
+++ app/[locale]/(dashboard)/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const t = await getTranslations("HomePage");
   return (
     <div>
-      <div className="px-6 text-center py-2 bg-gray-200">
+      <div className="px-6 text-center py-2 bg-gray-200 md:px-12">
         <p className="text-sm">
           Türkiye’de Nike.com ve Nike App üzerinden şu an için alışveriş
           yapılamamaktadır.
@@ -20,27 +20,37 @@ export default async function Home() {
           src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmplb2h2bzk1Z3ZrdzI4cHBwaTk2dm4wNWdsYm5lNTZqZGw4NDRzdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6Ztqh4JSlVqi2Z20/giphy.gif"
           alt="Nike Video"
           width={1920}
-          height={0}
-          className="z-[-9999]"
+          height={100}
+          className="z-[-9999] md:h-[150px] md:object-cover lg:h-[300px] xl:h-[500px]"
         />
-        <div className="absolute bottom-0 px-4 text-black py-4">
-          <p className=" text-xs">Satışta: Shox TL</p>
-          <h3 className="text-3xl font-extrabold">EZBER BOZAN TASARIM</h3>
-          <button className="bg-black text-white rounded-2xl w-[70px] py-1 text-sm">
+        <div className="absolute bottom-0 px-4 text-black py-4 md:top-0 md:py-0 lg:bottom-0 lg:top-24 xl:top-64">
+          <p className=" text-xs md:hidden lg:block lg:text-sm">
+            Satışta: Shox TL
+          </p>
+          <h3 className="text-3xl font-extrabold lg:text-5xl xl:text-7xl">
+            EZBER BOZAN TASARIM
+          </h3>
+          <p className="hidden md:block mt-4 lg:pr-[800px] xl:pr-[1440px]">
+            İkonik Shox TL, fütüristik silüetiyle zamanın ötesinde bir tasarım
+            ortaya koyuyor ve stil anlayışının sınırlarını zorluyor.
+          </p>
+          <button className="bg-black text-white rounded-2xl w-[70px] py-1 text-sm md:mt-4">
             İncele
           </button>
         </div>
       </div>
       <div className="px-5 mt-8">
-        <h1 className="text-3xl font-extrabold text-center">{t("title")}</h1>
-        <p className="text-center mt-2">{t("titleFirst")}</p>
+        <h1 className="text-3xl font-extrabold text-center md:text-4xl lg:text-5xl xl:text-7xl">
+          {t("title")}
+        </h1>
+        <p className="text-center mt-2 lg:px-72">{t("titleFirst")}</p>
         <p className="text-center  mt-4">
           Favori ürünlerini sana en yakın Nike mağazasında bulabilirsin.
           Mağazalarımıza ulaşamıyor ya da aradığın ürünü bulamıyor musun?
         </p>
         <p className="text-center">{t("titleSecond")}</p>
       </div>
-      <div className="flex flex-col mt-4 gap-2 items-center">
+      <div className="flex flex-col mt-4 gap-2 items-center md:flex-row md:justify-center">
         <button className="text-white bg-black w-[160px] rounded-2xl py-[2px]">
           Nike Mağazası Bul
         </button>
